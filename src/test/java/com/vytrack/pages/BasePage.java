aed3a35749e3f41ca630e8bc25ba5fcfb3f5012e
lbosimova14 <lbosimova@gmail.com>
2019-12-21 13:55:06 -0500

    base page changed

--- src/test/java/com/vytrack/pages/BasePage.java
+++ src/test/java/com/vytrack/pages/BasePage.java
@@ -11,7 +11,7 @@ import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
-
+//yyyyyyyyyyyyyyyyyyyyyy
 //git hum exercie
 //everything that is in common among pages
 //can go here
